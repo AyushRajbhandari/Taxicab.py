1f6c6add487f7f73d35798999114e45a9486968f
Ayush Rajbhandari <ayushrajbhandari06@gmail.com>
2026-06-30 17:56:57 -0700

    Implement Taxicab class for tracking coordinates
    Defines a Taxicab class with methods to track coordinates and odometer.

---
+++ Taxicab.py
@@ -0,0 +1,49 @@
+# Author: Ayush Rajbhandari
+# GitHub username: AyushRajbhandari
+# Date: 2026-06-30
+# Description: Defines a Taxicab class that tracks current x and y coordinates,
+#              along with an odometer reading that says the total distance traveled.
+
+class Taxicab:
+    """
+    A class representing a taxicab that keeps track of its current
+    x and y coordinates, as well as an odometer reading for total distance traveled.
+    """
+
+    def __init__(self, x_coord, y_coord):
+        """
+        Initializes a Taxicab object with the given x and y coordinates.
+        The odometer starts at 0
+        """
+        self._x_coord = x_coord
+        self._y_coord = y_coord
+        self._odometer = 0
+
+    def get_x_coord(self):
+        """Returns the current x-coordinate of the Taxicab."""
+        return self._x_coord
+
+    def get_y_coord(self):
+        """Returns the current y-coordinate of the Taxicab."""
+        return self._y_coord
+
+    def get_odometer(self):
+        """ Returns the current odometer of Taxicab."""
+        return self._odometer
+
+    def move_x(self, distance):
+        """
+        Updates the x-coordinate and adds the absolute value of the
+        Distance to the odometer.
+        """
+        self._x_coord += distance
+        self._odometer += abs(distance)
+
+    def move_y(self, distance):
+        """
+        Updates the y-coordinate and adds the absolute value of the
+        Distance to the odometer.
+        """
+        self._y_coord += distance
+        self._odometer += abs(distance)
+      
